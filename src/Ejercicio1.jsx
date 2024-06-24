cc724046c9503a14d1908950ab38dfb7aacc1303
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-06-24 16:46:55 -0300

    Act1 finalizada

--- src/Ejercicio1.jsx
+++ src/Ejercicio1.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
+
 export default function Ejercicio1() {
 
-  return (
-    <div>
-      <h2>Ejercicio 1: Lista desplegable</h2>
-
-      <label>Seleccione una fruta: </label>
-      {/* Crear lista de selección */}
-
-    </div>
-  )
-}
+    const [selectedFruit, setSelectedFruit] = useState('');
+  
+    const handleSelect = (event) => {
+      setSelectedFruit(event.target.value);
+    };
+  
+    return (
+      <div>
+        <select value={selectedFruit} onChange={handleSelect}>
+          <option value="">Seleccione una fruta</option>
+          <option value="Manzana">Manzana</option>
+          <option value="Pera">Pera</option>
+          <option value="Naranja">Naranja</option>
+          <option value="Mandarina">Mandarina</option>
+        </select>
+        <label>La fruta seleccionada actualmente es: {selectedFruit}</label>
+      </div>
+    );
+  };
